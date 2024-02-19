8cb00ff9acaf391cb7c0fce8ef4ffa0db458d5b6
Colin Smith <colin.kenjiro.smith@gmail.com>
2024-02-19 10:43:24 -0500

    first commit

---
+++ src/main/java/com/example/accessingdatajpa3/AccessingdataJpa3Application.java
@@ -0,0 +1,13 @@
+package com.example.accessingdatajpa3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingdataJpa3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessingdataJpa3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/accessingdatajpa3/AccessingdataJpa3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accessingdatajpa3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingdataJpa3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
